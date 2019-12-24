229295b386899fe5f9b99459cb17b1b478e7c636
Evgeny Yakimov <john.yakimov@gmail.com>
2019-12-24 17:13:25 +0000

    Add tracing to testclient
    Instantiate a global tracer that writes spans as json to file
    Instrument a span for the request itself
    No carrier support within the SimpleHttpClient component yet

--- src/testclient.cpp
+++ src/testclient.cpp
@@ -1,9 +1,25 @@
+#include <opentracing/mocktracer/json_recorder.h>
+#include <opentracing/mocktracer/tracer.h>
+
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <memory>
 
 #include "simplehttpclient.h"
 
-int main(int argc, char **argv) {
+using namespace opentracing;
+using namespace opentracing::mocktracer;
+
+std::shared_ptr<Tracer> create_file_json_tracer(const char* filename) {
+  // OpenTracing is very specific about the types of pointers.
+  return std::shared_ptr<Tracer>{new MockTracer{MockTracerOptions{
+      std::unique_ptr<Recorder>{new JsonRecorder{std::unique_ptr<std::ostream>{
+          new ofstream(filename, ios::out | ios::app)}}}}}};
+}
+
+int main(int argc, char** argv) {
+  // Check and consume args
   if (argc != 4) {
     std::cerr << "Usage: testclient <host> <port> <path>\n"
               << "Example:\n"
@@ -13,9 +29,24 @@ int main(int argc, char **argv) {
   string host = argv[1];
   int port = std::atoi(argv[2]);
   string path = argv[3];
+
+  // Instantiate global tracer
+  auto tracer = create_file_json_tracer("testclient.traces.json");
+  Tracer::InitGlobal(tracer);
+
+  // Instantiate the client
   auto client = SimpleHttpClient(host, port);
-  auto resp = client.get(path);
-  std::cout << "Response code: " << resp.code
-            << " data: " << resp.data.value_or("(None)") << std::endl;
+
+  // Make the request
+  {
+    // Life-time of span is only within this scope.
+    auto span = tracer->StartSpan("testclient.request");  
+    auto resp = client.get(path);
+    std::cout << "Response code: " << resp.code
+              << " data: " << resp.data.value_or("(None)") << std::endl;
+  }
+
+  // Close the tracer to collect spans
+  tracer->Close();
   return EXIT_SUCCESS;
 }
